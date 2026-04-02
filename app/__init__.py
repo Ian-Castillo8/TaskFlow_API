8d26b9310fecbe626c3b928a38b2aa47ec06e5b2
ian108-svg <ianrocast@gmail.com>
2026-04-01 20:10:35 -0400

    Add swagger UI and modify requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
+from flask_swagger_ui import get_swaggerui_blueprint
 from .models import db
 from config import Config
 
@@ -16,6 +17,14 @@ def create_app():
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(tasks_bp)
+
+    # Swagger UI
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        "/docs",
+        "/static/swagger.json",
+        config={"app_name": "TaskFlow API"}
+    )
+    app.register_blueprint(swaggerui_blueprint)
 
     with app.app_context():
         db.create_all()
